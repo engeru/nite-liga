0beb6b5e4d779043da9f7f961c71c84e63139f0e
Zorin Alexei <zetx43@gmail.com>
2018-02-08 20:06:14 +0300

    initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=zorin',
+    'username' => 'zorin',
+    'password' => 'Yegha8oh',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
