8cfada44eae7f2437c07b600c6c9503d01d345bc
Engeru <zetx43@gmail.com>
2018-02-15 21:43:45 +0400

    Added placeholders, and some shit don't know gonna merge next

---
+++ models/PlayerInTeam.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksei
+ * Date: 15.02.2018
+ * Time: 13:51
+ */
+
+namespace app\models;
+
+
+class PlayerInTeam
+{
+
+}
---
+++ views/site/reg-teams.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksei
+ * Date: 15.02.2018
+ * Time: 13:39
+ */
